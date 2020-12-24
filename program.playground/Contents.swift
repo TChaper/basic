d9f634f6aaa4ba9a89ee21ca30cb94f01931bde6
Tchaper <miracle777z@gmail.com>
2020-12-24 13:52:12 +0900

    성적 풀이

--- program.playground/Contents.swift
+++ program.playground/Contents.swift
@@ -20,3 +20,19 @@ func div(number1: Int, number2: Int) -> Int {
 }
 print(div(number1: 256, number2: 8))
 
+//프로그램 흐름 제어하기
+//연습문제 point를 넘기면 학점 “A”, “B”, “C”, “D”, “F”을 리턴하세요.
+func whatIsGrade(point : Int)-> Character {
+  if point >= 90 {
+    return "A"
+  } else if point >= 80 { //90점 미만 80점 이상
+    return "B"
+  } else if point >= 70 { //80점 미만 70점 이상
+    return "C"
+  } else if point >= 60 { //70점 미만 60점 이상
+    return "D"
+  } else { //60점 미만
+    return "E"
+  }
+}
+print(whatIsGrade(point: 100)) // A
