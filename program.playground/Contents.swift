a7e68e35be4c161b7414cece1bf47601db921fb0
Tchaper <miracle777z@gmail.com>
2020-12-24 13:51:34 +0900

    함수 풀이

---
+++ program.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+//직접 코딩해봐요
+func add(number1: Int, number2: Int) -> Int {
+  return number1 + number2
+}
+print(add(number1: 123, number2: 234))
+
+func sub(number1: Int, number2: Int) -> Int {
+  return number1 - number2
+}
+print(sub(number1: 456, number2: 123))
+
+func square(number: Int) -> Int {
+  return number * number
+}
+print(square(number: 8))
+
+func div(number1: Int, number2: Int) -> Int {
+  return number1 / number2
+}
+print(div(number1: 256, number2: 8))
+
