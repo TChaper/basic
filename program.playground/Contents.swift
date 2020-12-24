b4ff75640c029be2140558afd90b996dc4bdc520
Tchaper <miracle777z@gmail.com>
2020-12-24 14:07:54 +0900

    구구단: 2단과 3단

---
+++ program.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+print("2단과 3단 GUGUDAN")
+var a = 2 * 1
+print(a)
+var b = 2 * 2
+print(b)
+var c = 2 * 3
+print(c)
+var d = 2 * 4
+print(d)
+var e = 2 * 5
+print(e)
+var f = 2 * 6
+print(f)
+var g = 2 * 7
+print(g)
+var h = 2 * 8
+print(h)
+var i = 2 * 9
+print(i)
